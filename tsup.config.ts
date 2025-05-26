2d07be140ad493c197255180e9944d75e789d643
wookhyung <qpflapffhs76@gmail.com>
2025-05-26 09:57:36 +0900

    chore: migrate build setup to tsup and clean up tsconfig files

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  splitting: true,
+  external: [],
+  minify: false,
+  metafile: true,
+  outDir: 'dist',
+  target: 'es2020',
+  keepNames: true,
+});
